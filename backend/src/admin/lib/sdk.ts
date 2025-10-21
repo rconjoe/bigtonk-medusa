59517c0780947e67577ee1880054759d3f4532aa
rconjoe <root@trog.codes>
2025-10-21 12:38:09 -0700

    remove console log code

--- backend/src/admin/lib/sdk.ts
+++ backend/src/admin/lib/sdk.ts
@@ -4,12 +4,9 @@ import Medusa from "@medusajs/js-sdk";
 // const BACKEND_URL = "http://localhost:9000";
 
 const IS_DEV = process.env.NODE_ENV === "development";
-console.info(IS_DEV);
 
 // @ts-ignore
 const BACKEND_URL = import.meta.env.VITE_RAILWAY_PUBLIC_DOMAIN;
-
-console.log(BACKEND_URL);
 
 export const sdk = new Medusa({
   baseUrl: BACKEND_URL,
