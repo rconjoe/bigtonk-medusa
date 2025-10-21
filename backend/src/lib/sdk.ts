29e630ad39f18e1d4ced4b70694ffce7da2bbf9e
rconjoe <root@trog.codes>
2025-10-20 21:22:28 -0700

    fix BACKEND_URL and IS_DEV. use vite import.meta.env

--- backend/src/lib/sdk.ts
+++ backend/src/lib/sdk.ts
@@ -1,8 +1,15 @@
 import Medusa from "@medusajs/js-sdk";
 
+const IS_DEV = import.meta.env.NODE_ENV === "development";
+
+const BACKEND_URL =
+  import.meta.env.BACKEND_PUBLIC_URL ??
+  import.meta.env.RAILWAY_PUBLIC_DOMAIN ??
+  "http://localhost:9000";
+
 export const sdk = new Medusa({
-  baseUrl: "http://localhost:9000",
-  debug: true,
+  baseUrl: BACKEND_URL,
+  debug: IS_DEV,
   auth: {
     type: "session",
   },
