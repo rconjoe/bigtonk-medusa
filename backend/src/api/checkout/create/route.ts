6cc36fba4bfad23fc382b955f94f9bcdc6ffd5f1
rconjoe <root@trog.codes>
2025-11-09 16:47:25 -0800

    create checkout workflow

--- backend/src/api/checkout/create/route.ts
+++ backend/src/api/checkout/create/route.ts
@@ -1,19 +1,5 @@
 import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
-import { stripe } from "../../../lib/stripe";
-
-// const backend_url = process.env.BACKEND_URL;
-const frontend_url = process.env.FRONTEND_URL;
-
-// type CreateCheckoutRequestBody = Array<{
-//   price_data: {
-//     currency: string;
-//     product_data: {
-//       name: string;
-//     };
-//     unit_amount: number;
-//   };
-//   quantity: number;
-// }>;
+import { createCheckoutWorkflow } from "../../../workflows/create-checkout";
 
 type CartItem = {
   id: string;
@@ -29,28 +15,16 @@ type CartItem = {
 
 export async function POST(req: MedusaRequest, res: MedusaResponse) {
   try {
-    // MedusaRequest should already have the body parsed if the
-    // Content-Type is 'application/json' and body-parser is set up.
     const cartItems = req.body;
-    let line_items = [];
 
     console.log("Received cart items:", cartItems);
 
-    // @ts-ignore
-    cartItems.forEach((item: CartItem) => {
-      line_items.push({
-        price: item.stripe_price_id,
-        quantity: item.quantity,
-      });
+    const response = await createCheckoutWorkflow(req.scope).run({
+      // @ts-ignore
+      input: cartItems,
     });
 
-    const session = await stripe.checkout.sessions.create({
-      line_items,
-      mode: "payment",
-      ui_mode: "embedded",
-      return_url: `${frontend_url}/return?session_id={CHECKOUT_SESSION_ID}`,
-    });
-    res.status(200).json({ clientSecret: session.client_secret });
+    res.status(200).json(response);
   } catch (e) {
     console.error("Error in /checkout/create endpoint:", e);
     // You should send a more descriptive error if possible
